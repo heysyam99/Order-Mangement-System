61ad0bccd50e675b3af1eac3c66fb009c9dbe1fe
heysyam99 <hisyam@aerodyne.co>
2020-12-02 22:01:30 +0800

    [ADD] - Add configration for the env

---
+++ microservices/order-service/src/config.ts
@@ -0,0 +1,5 @@
+// tslint:disable: variable-name
+export const db_host = process.env.DB_HOST || 'localhost:27017';
+export const db_name = process.env.DB_NAME || 'ordermgmt';
+export const order_host = process.env.ORDER_HOST || '127.0.0.1';
+export const payment_host = process.env.PAYMENT_HOST || 'payment';
