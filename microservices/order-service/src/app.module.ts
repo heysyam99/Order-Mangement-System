7eeb3445f91361c7eb9012c77ade15b7212fc66f
heysyam99 <hisyam@aerodyne.co>
2020-12-02 22:11:58 +0800

    [IMPORT] - Import Order Module and Initialize Mongoose module for Order module

--- microservices/order-service/src/app.module.ts
+++ microservices/order-service/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { db_host, db_name } from './config';
+import { OrderModule } from './order/order.module';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(`mongodb://${db_host}/${db_name}`),
+    OrderModule,
+  ],
   controllers: [],
   providers: [],
 })
