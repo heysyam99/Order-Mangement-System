67560ebab1449620f7b1a0031aca8e6e7bc9ec2e
heysyam99 <hisyam@aerodyne.co>
2020-12-02 22:11:03 +0800

    [REMOVE] - Remove app service and controller from app module

--- microservices/order-service/src/app.module.ts
+++ microservices/order-service/src/app.module.ts
@@ -3,8 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
