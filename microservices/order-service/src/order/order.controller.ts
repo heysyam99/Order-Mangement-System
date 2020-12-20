2b73aa7c2c495d16dea1f7745bda93e0477b9a88
heysyam99 <hisyam@aerodyne.co>
2020-12-20 18:00:54 +0800

    [UPDATE] - Update create method to call payment service

--- microservices/order-service/src/order/order.controller.ts
+++ microservices/order-service/src/order/order.controller.ts
@@ -3,22 +3,37 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
+  HttpStatus,
+  Inject,
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
+import { ClientProxy, EventPattern } from '@nestjs/microservices';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    @Inject('ORDER_SERVICE') private readonly client: ClientProxy,
+  ) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  async create(@Res() res: Response, @Body() createOrderDto: CreateOrderDto) {
+    try {
+      const order = await this.orderService.create(createOrderDto);
+
+      this.client.emit('orderCreated', order.id);
+
+      return res.status(HttpStatus.CREATED).send(order);
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).send(JSON.stringify(error));
+    }
   }
 
   @Get()
@@ -42,5 +57,23 @@ export class OrderController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.orderService.remove(id);
+  }
+
+  @EventPattern('orderCreated')
+  async orderCreated(id: string) {
+    await this.orderService.initiatePayment(id);
+  }
+
+  @EventPattern('paymentProcessed')
+  async paymentProcessed(data) {
+    const order = await this.orderService.updatePaymentStatus(data);
+
+    if (order && order.status == 'Confirmed')
+      this.client.emit('orderConfirmed', order.id);
+  }
+
+  @EventPattern('orderConfirmed')
+  async orderConfirmed(id: string) {
+    await this.orderService.deliver(id);
   }
 }
