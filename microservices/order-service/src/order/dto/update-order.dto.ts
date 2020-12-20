a337f494f25e464e73dfc4ec628942d382432f30
heysyam99 <hisyam@aerodyne.co>
2020-12-20 18:02:20 +0800

    [UPDATE] - Add all api property for updating order dto

--- microservices/order-service/src/order/dto/update-order.dto.ts
+++ microservices/order-service/src/order/dto/update-order.dto.ts
@@ -1,4 +1,22 @@
-// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
 import { CreateOrderDto } from './create-order.dto';
 
-export class UpdateOrderDto {}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @IsNotEmpty()
+  @ApiProperty()
+  amount: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  username: string;
+
+  @IsNotEmpty()
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  transactionID: string;
+}
