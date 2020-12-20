382461190d96cc0706483b7fdc77a10fb505a3c4
heysyam99 <hisyam@aerodyne.co>
2020-12-20 17:52:16 +0800

    [UPDATE] - Create microservice for payment service

--- microservices/payment-service/src/main.ts
+++ microservices/payment-service/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
+import { Logger } from '@nestjs/common';
+import { payment_host } from './config';
+
+const logger = new Logger('Main');
+
+const microserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: payment_host,
+    port: 8875,
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+  app.listen(() => {
+    logger.log('Payment microservice is listening...');
+  });
 }
 bootstrap();
